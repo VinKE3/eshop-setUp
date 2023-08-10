e501c62c7fa8577b3b2db25c4e30dd696385648a
Kevin Muñoz <harquin09@gmail.com>
2023-08-10 12:15:23 -0500

    setUp

---
+++ libs/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVzx2zfI5ZwpiC6x4fj7akaB40FyXI7K0",
+  authDomain: "e-shop-394921.firebaseapp.com",
+  projectId: "e-shop-394921",
+  storageBucket: "e-shop-394921.appspot.com",
+  messagingSenderId: "43672102375",
+  appId: "1:43672102375:web:70775475b1512c52c0c942",
+  measurementId: "G-7MD60PR0YQ",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
